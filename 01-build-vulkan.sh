ee96e17babd841e2bf8206b4d3f1be0bc507f100
Nuno Santos <eu.nuno@gmail.com>
2026-06-11 09:57:17 +0100

    Update 01-build-vulkan.sh

--- 01-build-vulkan.sh
+++ 01-build-vulkan.sh
@@ -22,5 +22,5 @@ cmake -S . -B build -G Ninja \
     -DLLAMA_BUILD_EXAMPLES=OFF \
     -DLLAMA_BUILD_TESTS=OFF
 
-cmake --build build -j "$(nproc --ignore 2)"
+cmake --build build -j "$(nproc --ignore 2)" --target llama-diffusion-cli
 cmake --install build --strip
